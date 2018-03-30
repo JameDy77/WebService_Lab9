bc3c900c018accf5eca7f0ed823f3659b6599035
JameDy77 <58160702@go.buu.ac.th>
2018-03-30 13:14:48 +0700

    version 3 add route for students

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express();
 
 
 const subjectRoutes = require('./api/routes/subjects');
+const studentRoutes = require('./api/routes/students');
 
 
 app.use('/subjects', subjectRoutes);
+app.use('/students', studentRoutes);
 
 
 module.exports = app;
