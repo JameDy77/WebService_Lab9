88d8d3a23da4029d43bd963ee928e7500093e2ce
JameDy77 <58160702@go.buu.ac.th>
2018-03-30 13:16:55 +0700

    version 4 add morgan for easy check log

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+app.use(morgan('dev'));
 
 const subjectRoutes = require('./api/routes/subjects');
 const studentRoutes = require('./api/routes/students');
