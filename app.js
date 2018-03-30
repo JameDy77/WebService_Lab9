f1dcf2dec652ca6f4814ebf0234884a7f371cda8
JameDy77 <58160702@go.buu.ac.th>
2018-03-30 13:21:42 +0700

    version 8 add cosr for support remote call

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ app.use(bodyParser.json());
 const subjectRoutes = require('./api/routes/subjects');
 const studentRoutes = require('./api/routes/students');
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+  }); 
+
 app.use('/subjects', subjectRoutes);
 app.use('/students', studentRoutes);
 
