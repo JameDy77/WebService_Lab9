43df83752ba841d345b4d6dcac92213959731b90
JameDy77 <58160702@go.buu.ac.th>
2018-03-30 13:18:48 +0700

    version 5 add error handle for custom error message

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 app.use(morgan('dev'));
 
 const subjectRoutes = require('./api/routes/subjects');
 const studentRoutes = require('./api/routes/students');
 
-
 app.use('/subjects', subjectRoutes);
 app.use('/students', studentRoutes);
 
+app.use((req, res, next) => {
+   const error = new Error('Not found');
+   error.status = 404;
+   next(error);
+})
+
+app.use((error, req, res, next) => {
+   res.status(error.status || 500);
+   res.json({
+       error: {
+           message: error.message
+       }
+   });
+});
 
 module.exports = app;
